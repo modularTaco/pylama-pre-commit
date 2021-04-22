df51a4919d42d5f88b4074366de2fc1cfec028c5
modularTaco <37046961+modularTaco@users.noreply.github.com>
2021-04-22 10:39:18 +0200

    fix version to match git tag

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='pylama-pre-commit',
     description='Pylama pre-commit support',
     url='https://github.com/gvanderest/pylama-pre-commit',
-    version='0.2.0',
+    version='0.3.0',
     author='Guillaume VanderEst',
     author_email='gvanderest@gmail.com',
     install_requires=[
