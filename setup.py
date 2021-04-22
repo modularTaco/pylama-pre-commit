bcc636d2d21229d4217dd3d898204da8bc58bca0
modularTaco <37046961+modularTaco@users.noreply.github.com>
2021-04-22 10:38:51 +0200

    add missing dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='pylama-pre-commit',
     description='Pylama pre-commit support',
     url='https://github.com/gvanderest/pylama-pre-commit',
-    version='0.1.0',
+    version='0.2.0',
     author='Guillaume VanderEst',
     author_email='gvanderest@gmail.com',
     install_requires=[
@@ -17,6 +17,7 @@ setup(
         'pycodestyle',
         'pydocstyle',
         'mypy',
-        'eradicate'
+        'eradicate',
+        'pylama_pylint'
     ],
 )
